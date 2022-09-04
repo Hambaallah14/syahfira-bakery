25a35baefbf9bcc4076a8f7df0c8be3cf864d107
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-04 12:15:35 +0700

    Menambahkan View untuk Persediaan Barang

--- application/views/transaksi/persediaan_barang/index.php
+++ application/views/transaksi/persediaan_barang/index.php
@@ -1,1 +1,56 @@
-<h1>Persediaan Barang</h1>
+<div class="flash-data" data-target="Persediaan Barang" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
+<section class="content">
+    <div class="container-fluid">
+    
+        <div class="block-header">
+            <h2>Persediaan Barang</h2>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                
+                <div class="card">
+                    <div class="header">
+                            <a href="<?=base_url();?>transaksi/tambah_persediaan_barang" class="btn bg-brown">Tambah Persediaan Barang</a>
+                            
+                        <ul class="header-dropdown m-r--5">
+                            <li class="dropdown">
+                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <i class="material-icons">more_vert</i>
+                                </a>
+                                <ul class="dropdown-menu pull-right">
+                                    <li><a href="javascript:void(0);">Action</a></li>
+                                    <li><a href="javascript:void(0);">Another action</a></li>
+                                    <li><a href="javascript:void(0);">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped table-hover js-basic-example dataTable" ><!--dataTable js-exportable" -->
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Barang</th>
+                                        <th class="text-center">Qty</th>
+                                        <th class="text-center">Tanggal Transaksi</th>
+                                        <th class="text-center">Opsi</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <?php
+                                        $no=1;
+                                        
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
