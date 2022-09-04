b9b785e686c715d5da98c9826f40e22fb617d2b7
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-04 19:44:57 +0700

    Menambahkan View Persediaan Barang 2

--- application/views/transaksi/persediaan_barang/index.php
+++ application/views/transaksi/persediaan_barang/index.php
@@ -84,7 +84,10 @@
                                 <div class="form-group form-float">
                                     <div class="form-line">
                                         <input type="text" class="datepicker form-control tgl-transaksi" placeholder="Pilih Tanggal" name="tgl-transaksi" required>
-                                        <label class="form-label">Tanggal Transaksi</label>
+                                    </div>
+
+                                    <div class="form-line">
+                                        <input type="text" class="datepicker form-control" placeholder="Please choose a date...">
                                     </div>
                                 </div>
                             </div>
