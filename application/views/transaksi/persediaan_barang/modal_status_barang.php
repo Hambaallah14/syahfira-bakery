8eba7ebf6579245efdcba77b78e2c0ff6277f3a5
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-15 06:42:56 +0700

    Menambah modal 17

--- application/views/transaksi/persediaan_barang/modal_status_barang.php
+++ application/views/transaksi/persediaan_barang/modal_status_barang.php
@@ -1,44 +1,73 @@
-<div class="modal-header bg-teal">
+<div class="modal-header bg-pink">
     <h4 class="modal-title" id="defaultModalLabel" style="padding-top:-5px;padding-bottom:10px;color:white;">Memindai Persediaan Barang</h4>
 </div>
 <?php echo form_open("transaksi/add_status_barang", array('enctype'=>'multipart/form-data', 'id' => 'form_validation')); ?>                
 <div class="modal-body">
 
     <div class="body table-responsive">
-        <div class="form-group form-float">
-            <input type="hidden" name="id-transaksi" value="<?=$data_modal[0]["id_transaksi"];?>">
-            <div class="form-line">
-                <input type="text" class="form-control" name="id-barang" disabled value="<?=$data_modal[0]["id_barang"];?>">
-            </div>
-
-            <div class="form-line">
-                <input type="text" class="form-control" name="barang" disabled value="<?=$data_modal[0]["barang"];?>">
-            </div>
-
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group form-float">
-                        <div class="form-line">
-                            <input type="text" class="form-control" name="qty" disabled value="<?=$data_modal[0]["qty"]." ".$data_modal[0]["satuan"];?>">
+        <table class="table table-bordered">
+            <thead>
+                <input type="hidden" name="id-transaksi" value="<?=$data_modal[0]["id_transaksi"];?>"> 
+                <tr>
+                    <th>Pilih Menu</th>
+                    <th>:</th>
+                    <th>
+                        <div class="form-group form-float">
+                            <div class="form-line">
+                                <select class="form-control" name="pilih-menu">
+                                    <option value="">-Pilih Menu-</option>
+                                    <option value="Barang Keluar">Barang Keluar</option>
+                                    <option value="Barang Terjual">Barang Terjual</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    </th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td>ID Barang</td>
+                    <td>:</td>
+                    <td>
+                        <div class="form-group form-float">
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="id-barang" disabled value="<?=$data_modal[0]["id_barang"];?>">
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Barang</td>
+                    <td>:</td>
+                    <td>
+                        <div class="form-group form-float">
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="barang" disabled value="<?=$data_modal[0]["barang"];?>">
+                            </div>
+                        </div>
+                    </td>
+                </tr>
                 
-                <div class="col-md-6">
-                    <div class="form-group form-float">
-                        <div class="form-line">
-                            <input type="number" class="form-control" name="new-qty" required min="0" max="<?=$data_modal[0]["qty"];?>">
-                            <label class="form-label">Qty</label>
+                <tr>
+                    <td>Qty tersedia</td>
+                    <td>:</td>
+                    <td>
+                        <div class="form-group form-float">
+                            <div class="form-line">
+                                <input type="text" class="form-control" name="qty" disabled value="<?=$data_modal[0]["qty"]." ".$data_modal[0]["satuan"];?>">
+                            </div>
                         </div>
-                    </div> 
-                </div>
-            </div>
-        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </div>
         
 <div class="modal-footer">
-    <button type="submit" class="btn bg-teal waves-effect btn-simpan">Pindai Stok</button>
+    <button type="submit" class="btn bg-pink waves-effect btn-simpan">Pindai Stok</button>
     <?php echo form_close(); ?>
     <button type="button" class="btn btn-link waves-effect" data-dismiss="modal">Tutup</button>
 </div>
