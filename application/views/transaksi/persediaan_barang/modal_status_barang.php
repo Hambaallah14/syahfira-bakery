a23729d0cbd0dd1c011647ebd7a0a049a18f8d68
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-14 21:21:16 +0700

    Menambah modal 16

--- application/views/transaksi/persediaan_barang/modal_status_barang.php
+++ application/views/transaksi/persediaan_barang/modal_status_barang.php
@@ -6,8 +6,32 @@
 
     <div class="body table-responsive">
         <div class="form-group form-float">
+            <input type="hidden" name="id-transaksi" value="<?=$data_modal[0]["id_transaksi"];?>">
             <div class="form-line">
-                <input type="text" class="form-control" name="id-transaksi" disabled value="<?=$data_modal[0]["id_transaksi"];?>">
+                <input type="text" class="form-control" name="id-barang" disabled value="<?=$data_modal[0]["id_barang"];?>">
+            </div>
+
+            <div class="form-line">
+                <input type="text" class="form-control" name="barang" disabled value="<?=$data_modal[0]["barang"];?>">
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group form-float">
+                        <div class="form-line">
+                            <input type="text" class="form-control" name="qty" disabled value="<?=$data_modal[0]["qty"]." ".$data_modal[0]["satuan"];?>">
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6">
+                    <div class="form-group form-float">
+                        <div class="form-line">
+                            <input type="number" class="form-control" name="new-qty" required min="0" max="<?=$data_modal[0]["qty"];?>">
+                            <label class="form-label">Qty</label>
+                        </div>
+                    </div> 
+                </div>
             </div>
         </div>
     </div>
