2acacce2f28eba0c1f4cc627590958dd6cef1952
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-14 20:21:39 +0700

    Menambah modal 9

--- application/views/transaksi/barang_sisa/modal_barang_sisa.php
+++ application/views/transaksi/barang_sisa/modal_barang_sisa.php
@@ -1,5 +1,32 @@
-<?php
-
-foreach($data_modal as $data){
-    echo"<h5>".$data['barang']."</h5>";
-}
+<?php echo form_open("transaksi/add_barang_sisa", array('enctype'=>'multipart/form-data', 'id' => 'form_validation')); ?>
+                        
+    <div class="body table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <input type="text" class="modal-id-transaksi" name="id-transaksi" value="<?= $data_modal[0]["id_transaksi"];?>">
+                <input type="text" class="modal-id-barang" name="id-barang" value="<?= $data_modal[0]["id_barang"];?>">
+                <input type="text" class="modal-qty" name="qty" value="<?= $data_modal[0]["qty"];?>">
+                
+                <tr>
+                    <th>ID Barang</th>
+                    <th>:</th>
+                    <th><?php echo $data_modal[0]["id_barang"];?></th>
+                </tr>
+                
+            </thead>
+            
+            <tbody>
+                <tr>
+                    <td>Barang</td>
+                    <td>:</td>
+                    <td><?php echo $data_modal[0]["barang"];?></td>
+                </tr>
+                
+                <tr>
+                    <td>Qty</td>
+                    <td>:</td>
+                    <td><?php echo $data_modal[0]["qty"];?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>    
