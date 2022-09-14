e13e6c8e74ab41b13961db7795e3fab0fbb84c11
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-14 10:59:41 +0700

    Menambah modal2

--- application/views/transaksi/barang_sisa/modal_barang_sisa.php
+++ application/views/transaksi/barang_sisa/modal_barang_sisa.php
@@ -1,39 +1,8 @@
-<?php echo form_open("transaksi/add_barang_sisa", array('enctype'=>'multipart/form-data', 'id' => 'form_validation')); ?>
-                        
-    <div class="body table-responsive">
-        <table class="table table-bordered">
-            <thead>
-                <input type="text" class="modal-id-transaksi" name="id-transaksi">
-                <input type="text" class="modal-id-barang" name="id-barang">
-                <input type="text" class="modal-qty" name="qty">
-                
-                <tr>
-                    <th>ID Barang</th>
-                    <th>:</th>
-                    <th><div id="id-barang"></div></th>
-                </tr>
-                
-            </thead>
-            
-            <tbody>
-                <tr>
-                    <td>Barang</td>
-                    <td>:</td>
-                    <td><div id="barang"></div></td>
-                </tr>
-                
-                <tr>
-                    <td>Qty</td>
-                    <td>:</td>
-                    <td><div id="qty"></div></td>
-                </tr>
-            </tbody>
-        </table>
-    </div>      
+  
 
     <?php
     foreach($data_modal as $data){
-        echo $data["id_transaksi"];
-        echo $data["barang"];
+        echo"<h1>".$data["id_transaksi"]."</h1>";
+        echo"<h1>".$data["barang"]."</h1>";
     }
     ?>
