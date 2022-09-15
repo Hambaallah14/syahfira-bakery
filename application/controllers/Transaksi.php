7c6b8799fb710c5565c8d1f6c4174b68181705fb
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-15 12:21:55 +0700

    Menambah Barang Terjual

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -64,25 +64,19 @@ Class transaksi extends CI_Controller{
 		$this->form_validation->set_rules('pilih-menu', 'pilih-menu', 'required');
 		$this->form_validation->set_rules('id-barang', 'id-barang', 'required');
 		$this->form_validation->set_rules('new-qty', 'new-qty', 'required');
-        
-        $tanggal = date('Y-m-d');
-        $id_transaksi = $this->input->post('id-transaksi', true);
-        $id_barang = $this->input->post('id-barang', true);
-        $new_qty = $this->input->post('new-qty', true);
+    
 
-        echo"Tanggal ".$tanggal." ID-Transaksi : ".$id_transaksi.", ID-Barang : ".$id_barang.", Qty : ".$new_qty;
-
-		// if ($this->form_validation->run() == FALSE) {
-		// 	$this->load->view('template/header', $data);
-        //     $this->load->view('template/sidebar');
-        //     $this->load->view('transaksi/persediaan_barang/index');
-        //     $this->load->view('template/footer');
-		// }
-		// else{
-		// 	$this->Transaksi_Model->add_status_barang();
-		// 	$this->session->set_flashdata('flash', 'Disimpan');
-		// 	redirect('transaksi/persediaan_barang');
-		// }
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('template/header', $data);
+            $this->load->view('template/sidebar');
+            $this->load->view('transaksi/persediaan_barang/index');
+            $this->load->view('template/footer');
+		}
+		else{
+			$this->Transaksi_Model->add_status_barang();
+			$this->session->set_flashdata('flash', 'Disimpan');
+			redirect('transaksi/persediaan_barang');
+		}
     }
 
     // <!-- END PERSEDIAAN BARANG -->
