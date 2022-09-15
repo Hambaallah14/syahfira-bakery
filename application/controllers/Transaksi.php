ec1d8fdbbee8fa4c8f26cc2cb6549004e0540f63
Hambaallah14 <fahmiazri11@gmail.com>
2022-09-15 10:05:32 +0700

    Menambah Barang Terjual

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -58,6 +58,26 @@ Class transaksi extends CI_Controller{
         $this->load->view('transaksi/persediaan_barang/modal_status_barang', $data);
     }
 
+    public function add_status_barang(){
+        $data['title'] 	            = "Persediaan Barang - Syahfira Bakery & Cake";
+		$this->form_validation->set_rules('id-transaksi', 'id-transaksi', 'required');
+		$this->form_validation->set_rules('pilih-menu', 'pilih-menu', 'required');
+		$this->form_validation->set_rules('id-barang', 'id-barang', 'required');
+		$this->form_validation->set_rules('new-qty', 'new-qty', 'required');
+        
+		if ($this->form_validation->run() == FALSE) {
+			$this->load->view('template/header', $data);
+            $this->load->view('template/sidebar');
+            $this->load->view('transaksi/persediaan_barang/index');
+            $this->load->view('template/footer');
+		}
+		else{
+			$this->Transaksi_Model->add_status_barang();
+			$this->session->set_flashdata('flash', 'Disimpan');
+			redirect('transaksi/persediaan_barang');
+		}
+    }
+
     // <!-- END PERSEDIAAN BARANG -->
     
 
--- application/models/Transaksi_Model.php
+++ application/models/Transaksi_Model.php
@@ -27,6 +27,29 @@ class Transaksi_Model extends CI_Model{
       return $this->db->query("SELECT tb_persediaan_barang.id_transaksi, tb_persediaan_barang.id_barang, tb_persediaan_barang.qty, tb_persediaan_barang.tanggal_transaksi, tb_barang.barang, tb_satuan.satuan FROM tb_persediaan_barang INNER JOIN tb_barang ON tb_persediaan_barang.id_barang=tb_barang.id_barang INNER JOIN tb_satuan ON tb_satuan.id_satuan=tb_barang.id_satuan WHERE tb_persediaan_barang.id_transaksi='$id_transaksi'")->result_array();
     }
 
+    public function add_barang_sisa(){
+      $pilih_menu = $this->input->post('pilih-menu', true);
+      if($pilih_menu == "Barang Terjual"){
+        // ditambahkan ke barang terjual
+        $data_barang_terjual = [
+          "tanggal_transaksi"   => date('Y-m-d'),
+          "id_transaksi"        => $this->input->post('id-transaksi', true),
+          "id_barang"           => $this->input->post('id-barang', true),
+          "qty"                 => $this->input->post('new-qty', true)
+        ];
+        $this->db->insert('tb_barang_terjual', $data_barang_terjual);
+
+        // Update Barang Persediaan Barang
+        $this->db->set('qty', 'qty-'.$this->input->post('new-qty', true), true);
+        $this->db->where("id_transaksi", $this->input->post('id-barang', true));
+        $this->db->update('tb_persediaan_barang');
+      }
+      else{
+
+      }
+      
+    }
+
 
 
 
