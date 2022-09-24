a0f3a152471c6ee9833871d0950915a906e9ed70
hambaallah14 <fahmiazri11@gmail.com>
2022-09-24 15:27:59 +0700

    Membuat Script Rekap Laporan Persediaan Barang2

--- assets/new-script.js
+++ assets/new-script.js
@@ -1,30 +1,32 @@
-// PERSEDIAAN BARANG
-$('#rekap-persediaan-barang').change(function(){
-	var option = $(this).val();
-	if(option == "per-tanggal"){
-		$("#per-tanggal-persediaan-barang").show();
-		$("#per-bulan-persediaan-barang").hide();
-		$("#per-tahun-persediaan-barang").hide();
-	}
-	else if(option == "per-bulan"){
-		$("#per-bulan-persediaan-barang").show();
-		$("#per-tanggal-persediaan-barang").hide();
-		$("#per-tahun-persediaan-barang").hide();
-	}
-	else if(option == "per-tahun"){
-		$("#per-tahun-persediaan-barang").show();
-		$("#per-tanggal-persediaan-barang").hide();
-		$("#per-bulan-persediaan-barang").hide();
-	}
-});
+$(document).ready(function(){
+    // PERSEDIAAN BARANG
+    $('#rekap-persediaan-barang').change(function(){
+        var option = $(this).val();
+        if(option == "per-tanggal"){
+            $("#per-tanggal-persediaan-barang").show();
+            $("#per-bulan-persediaan-barang").hide();
+            $("#per-tahun-persediaan-barang").hide();
+        }
+        else if(option == "per-bulan"){
+            $("#per-bulan-persediaan-barang").show();
+            $("#per-tanggal-persediaan-barang").hide();
+            $("#per-tahun-persediaan-barang").hide();
+        }
+        else if(option == "per-tahun"){
+            $("#per-tahun-persediaan-barang").show();
+            $("#per-tanggal-persediaan-barang").hide();
+            $("#per-bulan-persediaan-barang").hide();
+        }
+    });
 
 
-// BARANG KELUAR
-
-
-
-// BARANG TERJUAL
+    // BARANG KELUAR
 
 
 
-// BARANG SISA
+    // BARANG TERJUAL
+
+
+
+    // BARANG SISA
+})
