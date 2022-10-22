5057904153035a8635fc5afc8dcc2305e40db0d6
Hambaallah14 <fahmiazri11@gmail.com>
2022-10-22 16:38:42 +0700

    Membuat menu pindai stok2

--- application/views/transaksi/persediaan_barang/pindaiStokBarang.php
+++ application/views/transaksi/persediaan_barang/pindaiStokBarang.php
@@ -56,7 +56,7 @@
                                 <div class="col-md-6">
                                     <div class="form-group form-float">
                                         <div class="form-line"> 
-                                            <input type="text" class="form-control" name="qty" readonly value="<?=$data_modal[0]["qty"]." ".$persediaan_barang[0]["satuan"];?>">     
+                                            <input type="text" class="form-control" name="qty" readonly value="<?=$persediaan_barang[0]["qty"]." ".$persediaan_barang[0]["satuan"];?>">     
                                         </div>
                                     </div>
                                 </div>
