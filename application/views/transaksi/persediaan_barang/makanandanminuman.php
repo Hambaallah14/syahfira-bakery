78f7619c7c0190a783a09a6ae6b114af6beb7f86
Hambaallah14 <fahmiazri11@gmail.com>
2023-01-31 10:25:48 +0700

    Membuat Tanggal Expired di persediaan makanan22

--- application/views/transaksi/persediaan_barang/makanandanminuman.php
+++ application/views/transaksi/persediaan_barang/makanandanminuman.php
@@ -124,7 +124,24 @@
                         <div class="col-md-12">
                             <div class="form-group form-float">
                                 <div class="form-line">
-                                    <input type="hidden" name="id_user" value="<?= $user[0]["id_user"]; ?>">
+                                    <select id="id_user" class="form-control show-tick id_user" name="id_user" required>
+                                        <option value="-">--Pilih Cabang--</option>
+                                        <?php
+                                        foreach ($daftar_user as $users) {
+                                            echo "<option value='" . $users["id_user"] . "'>" . $users['nama'] . "</option>";
+                                        }
+                                        ?>
+                                    </select>
+                                    <div id="harga"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-10">
+                            <div class="form-group form-float">
+                                <div class="form-line">
                                     <select id="id_barang" class="form-control show-tick id_barang" name="id_barang" required>
                                         <option value="-">--Pilih Barang--</option>
                                         <?php
@@ -133,8 +150,14 @@
                                         }
                                         ?>
                                     </select>
-
-                                    <div id="harga"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-group form-float">
+                                <div class="form-line">
+                                    <input type="number" class="form-control qty" name="qty" required min="0">
+                                    <label class="form-label">Qty</label>
                                 </div>
                             </div>
                         </div>
@@ -160,15 +183,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="col-md-3">
-                            <div class="form-group form-float">
-                                <div class="form-line">
-                                    <input type="number" class="form-control qty" name="qty" required min="0">
-                                    <label class="form-label">Qty</label>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-9">
+                        <div class="col-md-12">
                             <div class="form-group form-float">
                                 <div class="form-line">
                                     <input type="text" class="form-control ket" placeholder="Pilih Keterangan" name="ket" required>
