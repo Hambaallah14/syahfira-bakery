8b60e7e6c9e65ba1fac7b4e2d2576c88fadb3ef7
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-02 18:27:25 +0700

    Membuat Sidebar5

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -105,46 +105,37 @@
                     </li>
                 <?php } ?>
 
-
-                <li class="header">REKAP LAPORAN</li>
-                <li>
-                    <a href="javascript:void(0);" class="menu-toggle">
-                        <i class="material-icons" style="color:#24181a;">print</i>
-                        <span style="color:#24181a;">Cetak Laporan</span>
-                    </a>
-                    <ul class="ml-menu">
-                        <li>
-                            <a href="<?= base_url(); ?>rekap_laporan/persediaan_barang">
-                                <span class="icon-name mt-5">Persediaan Barang</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url(); ?>rekap_laporan/barang_keluar">
-                                <span class="icon-name mt-5">Barang Keluar</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url(); ?>rekap_laporan/barang_terjual">
-                                <span class="icon-name mt-5">Barang Terjual</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="<?= base_url(); ?>rekap_laporan/barang_sisa">
-                                <span class="icon-name mt-5">Barang Sisa</span>
-                            </a>
-                        </li>
-                        <!-- <li>  
-                                    <a href="<?= base_url(); ?>filter_laporan/persediaan_barang">
-                                        <span class="icon-name mt-5">Persediaan Barang</span>
-                                    </a>
-                                </li>
-                                <li>  
-                                    <a href="<?= base_url(); ?>filter_laporan/penjualan_barang">
-                                        <span class="icon-name mt-5">Penjualan Barang</span>
-                                    </a>
-                                </li> -->
-                    </ul>
-                </li>
+                <?php if ($user[0]["h_akses"] == "admin" || $user[0]["h_akses"] == "user") { ?>
+                    <li class="header">REKAP LAPORAN</li>
+                    <li>
+                        <a href="javascript:void(0);" class="menu-toggle">
+                            <i class="material-icons" style="color:#24181a;">print</i>
+                            <span style="color:#24181a;">Cetak Laporan</span>
+                        </a>
+                        <ul class="ml-menu">
+                            <li>
+                                <a href="<?= base_url(); ?>rekap_laporan/persediaan_barang">
+                                    <span class="icon-name mt-5">Persediaan Barang</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url(); ?>rekap_laporan/barang_keluar">
+                                    <span class="icon-name mt-5">Barang Keluar</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url(); ?>rekap_laporan/barang_terjual">
+                                    <span class="icon-name mt-5">Barang Terjual</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?= base_url(); ?>rekap_laporan/barang_sisa">
+                                    <span class="icon-name mt-5">Barang Sisa</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                <?php } ?>
             </ul>
         </div>
         <!-- #Menu -->
@@ -166,140 +157,56 @@
             <li role="presentation" class="active"><a href="#skins" data-toggle="tab">SKINS</a></li>
             <li role="presentation"><a href="#settings" data-toggle="tab">SETTINGS</a></li>
         </ul>
-        <div class="tab-content">
-            <div role="tabpanel" class="tab-pane fade in active in active" id="skins">
-                <ul class="demo-choose-skin">
-                    <li data-theme="red" class="active">
-                        <div class="red"></div>
-                        <span>Red</span>
+
+        <div role="tabpanel" class="tab-pane fade" id="settings">
+            <div class="demo-settings">
+                <p>GENERAL SETTINGS</p>
+                <ul class="setting-list">
+                    <li>
+                        <span>Report Panel Usage</span>
+                        <div class="switch">
+                            <label><input type="checkbox" checked><span class="lever"></span></label>
+                        </div>
                     </li>
-                    <li data-theme="pink">
-                        <div class="pink"></div>
-                        <span>Pink</span>
+                    <li>
+                        <span>Email Redirect</span>
+                        <div class="switch">
+                            <label><input type="checkbox"><span class="lever"></span></label>
+                        </div>
                     </li>
-                    <li data-theme="purple">
-                        <div class="purple"></div>
-                        <span>Purple</span>
+                </ul>
+                <p>SYSTEM SETTINGS</p>
+                <ul class="setting-list">
+                    <li>
+                        <span>Notifications</span>
+                        <div class="switch">
+                            <label><input type="checkbox" checked><span class="lever"></span></label>
+                        </div>
                     </li>
-                    <li data-theme="deep-purple">
-                        <div class="deep-purple"></div>
-                        <span>Deep Purple</span>
+                    <li>
+                        <span>Auto Updates</span>
+                        <div class="switch">
+                            <label><input type="checkbox" checked><span class="lever"></span></label>
+                        </div>
                     </li>
-                    <li data-theme="indigo">
-                        <div class="indigo"></div>
-                        <span>Indigo</span>
+                </ul>
+                <p>ACCOUNT SETTINGS</p>
+                <ul class="setting-list">
+                    <li>
+                        <span>Offline</span>
+                        <div class="switch">
+                            <label><input type="checkbox"><span class="lever"></span></label>
+                        </div>
                     </li>
-                    <li data-theme="blue">
-                        <div class="blue"></div>
-                        <span>Blue</span>
-                    </li>
-                    <li data-theme="light-blue">
-                        <div class="light-blue"></div>
-                        <span>Light Blue</span>
-                    </li>
-                    <li data-theme="cyan">
-                        <div class="cyan"></div>
-                        <span>Cyan</span>
-                    </li>
-                    <li data-theme="teal">
-                        <div class="teal"></div>
-                        <span>Teal</span>
-                    </li>
-                    <li data-theme="green">
-                        <div class="green"></div>
-                        <span>Green</span>
-                    </li>
-                    <li data-theme="light-green">
-                        <div class="light-green"></div>
-                        <span>Light Green</span>
-                    </li>
-                    <li data-theme="lime">
-                        <div class="lime"></div>
-                        <span>Lime</span>
-                    </li>
-                    <li data-theme="yellow">
-                        <div class="yellow"></div>
-                        <span>Yellow</span>
-                    </li>
-                    <li data-theme="amber">
-                        <div class="amber"></div>
-                        <span>Amber</span>
-                    </li>
-                    <li data-theme="orange">
-                        <div class="orange"></div>
-                        <span>Orange</span>
-                    </li>
-                    <li data-theme="deep-orange">
-                        <div class="deep-orange"></div>
-                        <span>Deep Orange</span>
-                    </li>
-                    <li data-theme="brown">
-                        <div class="brown"></div>
-                        <span>Brown</span>
-                    </li>
-                    <li data-theme="grey">
-                        <div class="grey"></div>
-                        <span>Grey</span>
-                    </li>
-                    <li data-theme="blue-grey">
-                        <div class="blue-grey"></div>
-                        <span>Blue Grey</span>
-                    </li>
-                    <li data-theme="black">
-                        <div class="black"></div>
-                        <span>Black</span>
+                    <li>
+                        <span>Location Permission</span>
+                        <div class="switch">
+                            <label><input type="checkbox" checked><span class="lever"></span></label>
+                        </div>
                     </li>
                 </ul>
             </div>
-            <div role="tabpanel" class="tab-pane fade" id="settings">
-                <div class="demo-settings">
-                    <p>GENERAL SETTINGS</p>
-                    <ul class="setting-list">
-                        <li>
-                            <span>Report Panel Usage</span>
-                            <div class="switch">
-                                <label><input type="checkbox" checked><span class="lever"></span></label>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Email Redirect</span>
-                            <div class="switch">
-                                <label><input type="checkbox"><span class="lever"></span></label>
-                            </div>
-                        </li>
-                    </ul>
-                    <p>SYSTEM SETTINGS</p>
-                    <ul class="setting-list">
-                        <li>
-                            <span>Notifications</span>
-                            <div class="switch">
-                                <label><input type="checkbox" checked><span class="lever"></span></label>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Auto Updates</span>
-                            <div class="switch">
-                                <label><input type="checkbox" checked><span class="lever"></span></label>
-                            </div>
-                        </li>
-                    </ul>
-                    <p>ACCOUNT SETTINGS</p>
-                    <ul class="setting-list">
-                        <li>
-                            <span>Offline</span>
-                            <div class="switch">
-                                <label><input type="checkbox"><span class="lever"></span></label>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Location Permission</span>
-                            <div class="switch">
-                                <label><input type="checkbox" checked><span class="lever"></span></label>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+        </div>
         </div>
     </aside>
     <!-- #END# Right Sidebar -->
