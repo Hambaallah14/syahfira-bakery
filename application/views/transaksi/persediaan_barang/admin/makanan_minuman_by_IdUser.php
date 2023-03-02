32627f6bd2a9cc996ffbb56bd9c75d342b734b0e
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-02 11:26:19 +0700

    Menambah Tanggal Expired6

--- application/views/transaksi/persediaan_barang/admin/makanan_minuman_by_IdUser.php
+++ application/views/transaksi/persediaan_barang/admin/makanan_minuman_by_IdUser.php
@@ -51,8 +51,8 @@
                                         echo "<td>" . $persediaan["qty"] . " " . $persediaan["satuan"] . "</td>";
                                         echo "<td class='text-center'>" . date('d F Y', strtotime($persediaan["tanggal_transaksi"])) . "</td>";
 
-                                        $tanggal_1 = date('Y-m-d', strtotime('+' . $persediaan["durasi_exp"] . 'days', strtotime($persediaan["tanggal_transaksi"])));
-                                        // echo "<td class='text-center'>" . date('d F Y', strtotime($persediaan["tgl_expired"])) . "</td>";
+                                        $tanggal_expired = date('d F Y', strtotime('+' . $persediaan["durasi_exp"] . 'days', strtotime($persediaan["tanggal_transaksi"])));
+                                        echo "<td class='text-center'>" . $tanggal_expired . "</td>";
                                         echo "<td class='text-center'>" . $persediaan["ket"] . "</td>";
 
                                         // JIKA STATUS PERSANAN MASIH PROSES
