bca65228c0671405800f11883238eadfe7e1d5f9
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-31 10:50:33 +0700

    Mengubah41

--- application/views/bahan_baku/persediaan/user/index.php
+++ application/views/bahan_baku/persediaan/user/index.php
@@ -58,51 +58,7 @@
                                         echo "<td>" . $persediaan["qty"] . " " . $persediaan["satuan"] . "</td>";
                                         echo "<td class='text-center'>" . date('d F Y', strtotime($persediaan["tgl_persediaan"])) . "</td>";
 
-                                        $tanggal_expired = date('d F Y', strtotime('+' . $persediaan["durasi_expired"] . 'days', strtotime($persediaan["tgl_persediaan"])));
 
-                                        echo "<td class='text-center'>" . $tanggal_expired . "</td>";
-                                        echo "<td class='text-center'>" . $persediaan["keterangan"] . "</td>";
-
-                                        $tanggal_expired2 = date('Y-m-d', strtotime('+' . $persediaan["durasi_expired"] . 'days', strtotime($persediaan["tgl_persediaan"])));
-                                        if (date('Y-m-d') > $tanggal_expired2) {
-                                            echo "<td class='text-center'>";
-                                            echo "<button type='button' class='btn btn-danger waves-effect'>";
-                                            echo "<i class='material-icons'>cancel</i>";
-                                            echo "<span>EXPIRED</span>";
-                                            echo "</button>";
-                                            echo "</td>";
-                                        } else {
-                                            // JIKA PESANAN DI PROSES
-                                            if ($persediaan["status_persediaan"] == 0) {
-                                                echo "<td class='text-center'>";
-                                                echo "<button type='button' class='btn btn-primary waves-effect btnPesananPersediaanBahanBaku' data-toggle='modal' data-target='#defaultModal' data-id='" . $persediaan["id_persediaan"] . "'>";
-                                                echo "<i class='material-icons'>query_builder</i>";
-                                                echo "<span>PESANAN</span>";
-                                                echo "</button>";
-                                                echo "</td>";
-                                            }
-
-                                            // JIKA PESANAN DITERIMA
-                                            else if ($persediaan["status_persediaan"] == 1) {
-                                                echo "<td class='text-center'>";
-                                                echo "<button type='button' class='btn btn-success waves-effect'>";
-                                                echo "<i class='material-icons'>done</i>";
-                                                echo "<span>PESANAN DITERIMA</span>";
-                                                echo "</button>";
-                                                echo "</td>";
-                                            }
-
-                                            // JIKA PESANAN DITOLAK
-                                            else if ($persediaan["status_persediaan"] == 2) {
-                                                echo "<td class='text-center'>";
-                                                echo "<button type='button' class='btn btn-danger waves-effect' data-container='body' data-toggle='popover'
-                                                data-placement='top' title='Informasi' data-content='" . $persediaan["status_keterangan"] . "'>";
-                                                echo "<i class='material-icons'>cancel</i>";
-                                                echo "<span>PESANAN DITOLAK</span>";
-                                                echo "</button>";
-                                                echo "</td>";
-                                            }
-                                        }
 
                                         echo "</tr>";
                                         $no++;
