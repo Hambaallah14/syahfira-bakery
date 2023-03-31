8a40296e340a3d4bce718d369262b7fd8180a6cb
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-31 16:46:38 +0700

    Mengubah58

--- application/views/makanan_dan_minuman/persediaan/user/pindah_stok.php
+++ application/views/makanan_dan_minuman/persediaan/user/pindah_stok.php
@@ -27,8 +27,31 @@
                     <div class="body">
                         <?php echo form_open("makanan_dan_minuman/InsertBarangSisa", array('enctype' => 'multipart/form-data', 'id' => 'form_validation')); ?>
                         <input type="text" name="id_user" value="<?= $user[0]["id_user"]; ?>">
-                        <input type="text" name="id_persediaan" value="<?= $makanan_minuman[0]["id_persediaan"]; ?>">
-
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control" name="id_persediaan" readonly value="<?= $makanan_minuman[0]["id_persediaan"]; ?>">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control" name="harga" readonly value="<?= $makanan_minuman[0]["harga"]; ?>">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group form-float">
+                                    <div class="form-line">
+                                        <input type="text" class="form-control" name="qty" readonly value="<?= $makanan_minuman[0]["qty"]; ?>">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="row">
                             <div class="col-md-12">
                                 <button type="submit" class="btn bg-teal waves-effect btn-simpan">Pindai Stok</button>
