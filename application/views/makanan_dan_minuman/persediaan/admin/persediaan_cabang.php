db790468dadf40533aa5e52822fbed23963e8997
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-31 09:40:07 +0700

    Mengubah36

--- application/views/makanan_dan_minuman/persediaan/admin/persediaan_cabang.php
+++ application/views/makanan_dan_minuman/persediaan/admin/persediaan_cabang.php
@@ -58,7 +58,53 @@
                                         echo "<td class='text-center'>" . $tanggal_expired . "</td>";
                                         echo "<td class='text-center'>" . $persediaan["keterangan"] . "</td>";
 
+                                        if (date('Y-m-d') > $tanggal_expired) {
+                                            echo "<td class='text-center'>";
+                                            echo "<button type='button' class='btn btn-danger waves-effect'>";
+                                            echo "<i class='material-icons'>cancel</i>";
+                                            echo "<span>EXPIRED</span>";
+                                            echo "</button>";
+                                            echo "</td>";
+                                        } else {
+                                            if ($persediaan["qty"] == 0) {
+                                                echo "<td class='text-center'>";
+                                                echo "<button type='button' class='btn btn-danger waves-effect'>";
+                                                echo "<i class='material-icons'>cancel</i>";
+                                                echo "<span>STOK HABIS</span>";
+                                                echo "</button>";
+                                                echo "</td>";
+                                            } else {
+                                                if ($persediaan["status_persediaan"] == 0) {
+                                                    echo "<td class='text-center'>";
+                                                    echo "<button type='button' class='btn btn-primary waves-effect'>";
+                                                    echo "<i class='material-icons'>query_builder</i>";
+                                                    echo "<span>PESANAN DIPROSES</span>";
+                                                    echo "</button>";
+                                                    echo "</td>";
+                                                }
 
+                                                // JIKA PESANAN DITERIMA
+                                                else if ($persediaan["status_persediaan"] == 1) {
+                                                    echo "<td class='text-center'>";
+                                                    echo "<button type='button' class='btn btn-success waves-effect'>";
+                                                    echo "<i class='material-icons'>done</i>";
+                                                    echo "<span>PESANAN DITERIMA</span>";
+                                                    echo "</button>";
+                                                    echo "</td>";
+                                                }
+
+                                                // JIKA PESANAN DITOLAK
+                                                else if ($persediaan["status_persediaan"] == 2) {
+                                                    echo "<td class='text-center'>";
+                                                    echo "<button type='button' class='btn btn-danger waves-effect' data-container='body' data-toggle='popover'
+                                                    data-placement='top' title='Informasi' data-content='" . $persediaan["status_keterangan"] . "'>";
+                                                    echo "<i class='material-icons'>cancel</i>";
+                                                    echo "<span>PESANAN DITOLAK</span>";
+                                                    echo "</button>";
+                                                    echo "</td>";
+                                                }
+                                            }
+                                        }
 
                                         echo "</tr>";
                                         $no++;
