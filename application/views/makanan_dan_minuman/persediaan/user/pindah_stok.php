0156f21c603e63db83fa405c753808061436c86a
Hambaallah14 <fahmiazri11@gmail.com>
2023-03-31 16:39:19 +0700

    Mengubah57

--- application/views/makanan_dan_minuman/persediaan/user/pindah_stok.php
+++ application/views/makanan_dan_minuman/persediaan/user/pindah_stok.php
@@ -1,1 +1,44 @@
-<h1><?php echo $makanan_minuman[0]["makanan_minuman"]; ?></h1>
+<section class="content">
+    <div class="container-fluid">
+
+        <div class="block-header">
+            <h2>Memindai Persediaan Makanan dan Minuman</h2>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <div class="card">
+                    <div class="header">
+                        <ul class="header-dropdown m-r--5">
+                            <li class="dropdown">
+                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <i class="material-icons">more_vert</i>
+                                </a>
+                                <ul class="dropdown-menu pull-right">
+                                    <li><a href="javascript:void(0);">Action</a></li>
+                                    <li><a href="javascript:void(0);">Another action</a></li>
+                                    <li><a href="javascript:void(0);">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="body">
+                        <?php echo form_open("makanan_dan_minuman/InsertBarangSisa", array('enctype' => 'multipart/form-data', 'id' => 'form_validation')); ?>
+                        <input type="text" name="id_user" value="<?= $user[0]["id_user"]; ?>">
+                        <input type="text" name="id_persediaan" value="<?= $makanan_minuman[0]["id_persediaan"]; ?>">
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn bg-teal waves-effect btn-simpan">Pindai Stok</button>
+                            </div>
+                        </div>
+                    </div>
+                    <?php echo form_close(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
