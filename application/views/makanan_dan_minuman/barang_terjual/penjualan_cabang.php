fe5f007282fc4e37886082934f4b08297dad6aaa
Hambaallah14 <fahmiazri11@gmail.com>
2023-04-01 07:45:46 +0700

    Mengubah92

--- application/views/makanan_dan_minuman/barang_terjual/penjualan_cabang.php
+++ application/views/makanan_dan_minuman/barang_terjual/penjualan_cabang.php
@@ -33,7 +33,6 @@
                                     <tr>
                                         <th class="text-center">No</th>
                                         <th class="text-center">Tanggal Penjualan</th>
-                                        <th class="text-center">Cabang</th>
                                         <th class="text-center">ID Persediaan</th>
                                         <th class="text-center">Makanan / Minuman</th>
                                         <th class="text-center">Harga</th>
@@ -48,9 +47,10 @@
                                         echo "<tr>";
                                         echo "<td>" . $no . "</td>";
                                         echo "<td>" . date('d F Y', strtotime($persediaan["tanggal"])) . "</td>";
-                                        echo "<td>" . $persediaan["nama"] . "</td>";
                                         echo "<td>" . $persediaan["id_persediaan"] . "</td>";
                                         echo "<td>" . $persediaan["makanan_minuman"] . "</td>";
+                                        echo "<td>Rp. " . number_format($persediaan["harga"], 0, ',', '.') . "</td>";
+                                        echo "<td>" . $persediaan["qty"] . " " . $persediaan["satuan"] . "</td>";
 
                                         echo "</tr>";
                                         $no++;
