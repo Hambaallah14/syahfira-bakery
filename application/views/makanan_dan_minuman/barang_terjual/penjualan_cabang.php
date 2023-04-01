268a6b1f63f0360e9ceec23e24eb3e6564201390
Hambaallah14 <fahmiazri11@gmail.com>
2023-04-01 07:41:12 +0700

    Mengubah90

---
+++ application/views/makanan_dan_minuman/barang_terjual/penjualan_cabang.php
@@ -0,0 +1,54 @@
+<section class="content">
+    <div class="container-fluid">
+
+        <div class="block-header">
+            <h2>Penjualan Makanan dan Minuman</h2>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <div class="card">
+                    <div class="header">
+                        <ul class="header-dropdown m-r--5">
+                            <li class="dropdown">
+                                <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <i class="material-icons">more_vert</i>
+                                </a>
+                                <ul class="dropdown-menu pull-right">
+                                    <li><a href="javascript:void(0);">Action</a></li>
+                                    <li><a href="javascript:void(0);">Another action</a></li>
+                                    <li><a href="javascript:void(0);">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                                <!--UNTUK ADMIN -->
+
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">No</th>
+                                        <th class="text-center">Tanggal Penjualan</th>
+                                        <th class="text-center">Cabang</th>
+                                        <th class="text-center">ID Persediaan</th>
+                                        <th class="text-center">Makanan / Minuman</th>
+                                        <th class="text-center">Harga</th>
+                                        <th class="text-center">Qty</th>
+                                        <th class="text-center">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
