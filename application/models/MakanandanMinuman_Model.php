e3027942a800fbe891c4b285f024b8f52c7e2c00
Hambaallah14 <fahmiazri11@gmail.com>
2023-06-08 20:39:38 +0700

    Mengubah Validasi Persediaan6

--- application/models/MakanandanMinuman_Model.php
+++ application/models/MakanandanMinuman_Model.php
@@ -152,40 +152,8 @@ class MakanandanMinuman_Model extends CI_Model
 
     public function InsertPersediaanSisa()
     {
-        foreach ($this->allPersediaanSisa() as $barang_sisa) {
-            // CEK APAKAH DATA BARANG SISA SAMA DENGAN BARANG YANG MAU DIMASUKKAN DARI PERSEDIAAN
-            if ($barang_sisa["id_persediaan"] == $this->input->post('id_persediaan', true)) {
-                $status = [
-                    "id_persediaan"         => $this->input->post('id_persediaan', true),
-                    "id_user"               => $this->input->post('id_user', true),
-                    "status_persediaan"     => 3
-                ];
-                $this->db->where("id_persediaan", $this->input->post('id_persediaan', true));
-                $this->db->update('tb_status_persediaan_mm', $status);
-            }
-            // KALO TIDAK TAMBAHKAN DATA KE BARANG SISA
-            else {
-                $status = [
-                    "id_persediaan"         => $this->input->post('id_persediaan', true),
-                    "id_user"               => $this->input->post('id_user', true),
-                    "status_persediaan"     => 3
-                ];
-                $this->db->where("id_persediaan", $this->input->post('id_persediaan', true));
-                $this->db->update('tb_status_persediaan_mm', $status);
-
-
-                $persediaan = [
-                    "tanggal"          => date('Y-m-d'),
-                    "id_persediaan"    => $this->input->post('id_persediaan', true),
-                    "id_user"          => $this->input->post('id_user', true),
-                    "id_makan_minum"   => $this->input->post('id_mkn_mnm', true),
-                    "harga"            => $this->input->post('harga', true),
-                    "qty"              => $this->input->post('qty', true),
-                    "tgl_persediaan"   => $this->input->post('tgl_persediaan', true)
-                ];
-                $this->db->insert('tb_barang_sisa', $persediaan);
-            }
-        }
+        $barang_sisa = $this->db->get('tb_barang_sisa');
+        var_dump($barang_sisa);
     }
 
     public function allPersediaanSisa()
